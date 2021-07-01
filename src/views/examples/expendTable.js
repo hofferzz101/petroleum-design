7d4ddaab9b1f423461849089cebdbeb6deeb9130
Misbah <syed19febali@gmail.com>
2021-07-01 15:17:32 +0500

    Terminal Completed

--- src/views/examples/expendTable.js
+++ src/views/examples/expendTable.js
@@ -1,191 +1,116 @@
-import React, { useEffect, useState } from "react"
-import PropTypes from "prop-types"
-import { makeStyles } from "@material-ui/core/styles"
-import Box from "@material-ui/core/Box"
-import Collapse from "@material-ui/core/Collapse"
-import IconButton from "@material-ui/core/IconButton"
-import Table from "@material-ui/core/Table"
-import TableBody from "@material-ui/core/TableBody"
-import TableCell from "@material-ui/core/TableCell"
-import TableContainer from "@material-ui/core/TableContainer"
-import TableHead from "@material-ui/core/TableHead"
-import TableRow from "@material-ui/core/TableRow"
-import Typography from "@material-ui/core/Typography"
-import Paper from "@material-ui/core/Paper"
-import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown"
-import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp"
-import { ContactSupportOutlined } from "@material-ui/icons"
-import { GET } from "configuration/API-Instance"
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import { GET, UPDATE } from "configuration/API-Instance"
+import moment from "moment"
 
-import EditOutlinedIcon from "@material-ui/icons/EditOutlined"
-import VisibilityOutlinedIcon from "@material-ui/icons/VisibilityOutlined"
-import DeleteOutlineOutlinedIcon from "@material-ui/icons/DeleteOutlineOutlined"
-
-import QRCode from "react-qr-code"
-
-import { saveSvgAsPng } from "save-svg-as-png"
-import "./pages/expendTable.css"
-// reactstrap components
-import {
-  Card,
-  FormGroup,
-  Label,
-  Form,
-  Input,
-  Row,
-  Modal,
-  Button,
-  ModalFooter,
-  ModalHeader,
-  ModalBody,
-  Container,
-} from "reactstrap"
 
 const useRowStyles = makeStyles({
   root: {
-    "& > *": {
-      borderBottom: "unset",
+    '& > *': {
+      borderBottom: 'unset',
     },
   },
-})
+});
 
+function createData(name, calories, fat, carbs, protein, price) {
+  return {
+    name,
+    calories,
+    fat,
+    carbs,
+    protein,
+    price,
+    history: [
+      { date: '2020-01-05', customerId: '11091700', amount: 3 },
+      { date: '2020-01-02', customerId: 'Anonymous', amount: 1 },
+    ],
+  };
+}
 
-
-function Rows(props) {
-  const { row } = props
-  const [open, setOpen] = React.useState(false)
-  const classes = useRowStyles()
-
-  let array = []
-  if (row) {
-    let product = row.Product
-    let gallons = row.Gallons
-    let destination = row.Destination
-    let shipTo = row.ShipTo
-
-    product.map((item, i) => {
-      array.push({
-        Product: item,
-        Gallons: gallons[i],
-        Destination: destination[i],
-        ShipTo: shipTo[i],
-      })
-    })
-  }
-
-
-  const [modal, setModal] = useState(false)
-  const toggle = () => setModal(!modal)
-
-  const downloadQRCode = () => {
-    saveSvgAsPng(document.getElementById("id"), row.order_number + "-qr.png")
-  }
-
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+  const classes = useRowStyles();
+   console.log(row)
   return (
     <React.Fragment>
-      <Modal isOpen={modal} toggle={toggle}>
-        <ModalHeader>Order no: {row ? row.order_number : ""}</ModalHeader>
-        <ModalBody style={{ textAlign: "center" }}>
-          <div>
-
-          </div>
-        </ModalBody>
-        <Button onClick={downloadQRCode}>Download QRCode</Button>
-      </Modal>
-
-      <TableRow id="opening-row-collapse" onClick={()=>setOpen(!open)} style={{}} className={classes.root}>
+      <TableRow className={classes.root}>
         <TableCell>
-          <IconButton
-            aria-label="expand row"
-            size="small"
-            onClick={() => setOpen(!open)}
-          >
+          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
           </IconButton>
         </TableCell>
-        <TableCell>{row.Contract}</TableCell>
-        <TableCell component="th" scope="row">
-          {row["CustomerNumber"]}
-        </TableCell>
-        <TableCell>{row.order_number}</TableCell>
-        <TableCell>{row["HaulerNumber"]}</TableCell>
-        <TableCell>{row["DriverNumber"]}</TableCell>
-        <TableCell>{row["PONumber"]}</TableCell>
-        <TableCell>{row["SupplierNumber"]}</TableCell>
-        <TableCell>{row["TerminalNumber"]}</TableCell>
-        <TableCell>{row["createddate"]}</TableCell>
-        <TableCell>{row["expirationdate"]}</TableCell>
-        {/* <TableCell style={{ width: 280 }}>
-          <a style={{ cursor: "pointer" }} className="material-icons-outlined">
-            <VisibilityOutlinedIcon />
-          </a>
-          &nbsp; &nbsp;
-          <a style={{ cursor: "pointer" }} className="material-icons-outlined">
-            <EditOutlinedIcon />
-          </a>{" "}
-          &nbsp; &nbsp;
-          <a style={{ cursor: "pointer" }} className="material-icons-outlined">
-            <DeleteOutlineOutlinedIcon />
-          </a>{" "}
-          &nbsp; &nbsp;
-          <Button color="primary" variant="contained" >
-            QR Code
-          </Button>
-        </TableCell> */}
+        <TableCell align="center">{"-"}</TableCell>
+        <TableCell align="center">{row.customer.number}</TableCell>
+        <TableCell align="center">{row.number}</TableCell>
+        <TableCell align="center">{row.customer.city}</TableCell>
+        <TableCell align="center">{row.p_no}</TableCell>
+        <TableCell align="center">{row.location}</TableCell>
+        <TableCell align="center">{moment(row.orderDate).format("DD/MM/YY")}</TableCell>
+        <TableCell align="center">{moment(row.deliveryDate).format("DD/MM/YY")}</TableCell>
+        <TableCell align="center">{row.order_date}</TableCell>
+        <TableCell align="center">{row.delivery_window}</TableCell>
+        <TableCell align="center">{row.status}</TableCell>
+        <TableCell align="center">{row.alerts}</TableCell>
+        
+        
       </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
-          <div className="table-styling">
             <Box margin={1}>
               <Typography variant="h6" gutterBottom component="div">
-                Products
+                Order Details
               </Typography>
-              
               <Table size="small" aria-label="purchases">
                 <TableHead>
                   <TableRow>
-                    <TableCell>Product</TableCell>
-                    <TableCell>Gallons</TableCell>
-                    <TableCell>Ship To</TableCell>
-                    <TableCell>Destination</TableCell>
+                    <TableCell align="center">Quantity</TableCell>
+                    <TableCell align="center">Order Id</TableCell>
+                    <TableCell align="center">Dilevery Date</TableCell>
+                    <TableCell align="center">Status</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {array.length
-                    ? array.map((inner, j) => (
-                      <>
-                      
-                        <TableRow key={j}>
-                          <TableCell>{inner.Product}</TableCell>
-                          <TableCell>{inner.Gallons}</TableCell>
-                          <TableCell>{inner.ShipTo}</TableCell>
-                          <TableCell>{inner.Destination}</TableCell>
-                        </TableRow>
-                        
-                      </>
-                    ))
-                    : null}
+                   {console.log(row.order_detials)}
+                  {row.order_detials.map((details,i) => (
+                    <TableRow key={i}>
+                      <TableCell align="center">
+                        {details.quantity}
+                      </TableCell>
+                      <TableCell align="center">{details.order_id}</TableCell>
+                      <TableCell align="center">{moment(details.dilivery_date).format("DD-MM-YY, h:mm:ss a")}</TableCell>
+                      <TableCell align="center">{details.status}</TableCell>
+                        {/* <TableCell align="center">
+                         {Math.round(historyRow.amount * row.price * 100) / 100}
+                        </TableCell> */}
+                    </TableRow>
+                  ))}
                 </TableBody>
               </Table>
-              
             </Box>
-            </div>
-            
           </Collapse>
         </TableCell>
-        {/* {
-          open ?
-            <div className="qr-design">
-              <QRCode className="qr-code-design" id="id" size={170} value={row ? JSON.stringify(row) : ""} />
-            </div> : null
-        } */}
       </TableRow>
     </React.Fragment>
-  )
+  );
 }
 
-Rows.propTypes = {
+Row.propTypes = {
   row: PropTypes.shape({
     calories: PropTypes.number.isRequired,
     carbs: PropTypes.number.isRequired,
@@ -195,79 +120,34 @@ Rows.propTypes = {
         amount: PropTypes.number.isRequired,
         customerId: PropTypes.string.isRequired,
         date: PropTypes.string.isRequired,
-      })
+      }),
     ).isRequired,
     name: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
     protein: PropTypes.number.isRequired,
   }).isRequired,
-}
+};
+
 
 export default function CollapsibleTable() {
-  const [MasterData, setMasterData] = useState([])
-  // const [openingModal, setOpeningModal] = useState(false)
-  const [getOrdersData, setGetOrdersData] = useState("")
-  console.log("master",MasterData)
 
-  useEffect(() => {
-    let data = JSON.parse(localStorage.getItem("Petroleum_Item"))
-    console.log(data)
-    if (data != null && data.length > 0) {
-      let products = data ? data[0].Product.split(",") : []
-      let gallons = data ? data[0].Gallons.split(",") : []
-      let shipTo = data ? data[0].Shipto.toString().split("/") : []
-      let destination = data ? data[0].Destination.split(",") : []
+  const [orderData, setOrderData] = useState([])
 
-      let formatedArray = []
 
-      for (let i = 0; i < data.length; i++) {
-        for (let j = 0; j < data.length; j++) {
-          formatedArray.push({
-            Product: products,
-            Gallons: gallons,
-            ShipTo: shipTo,
-            Destination: destination,
-            Contract: data[i]["Contract"],
-            CustomerNumber: data[i]["Customer Number"],
-            DriverNumber: data[i]["Driver Number"],
-            HaulerNumber: data[i]["Hauler Number"],
-            PONumber: data[i]["PO Number"],
-            PinCode: data[i]["Pin Code"],
-            SupplierNumber: data[i]["Supplier Number"],
-            TerminalNumber: data[i]["Terminal Number"],
-            Trailer: data[i]["Trailer"],
-            TruckNumber: data[i]["Truck Number"],
-            createddate: data[i]["created date"],
-            effectivedate: data[i]["effectiv edate"],
-            expirationdate: data[i]["expiration date"],
-            opendate: data[i]["open date"],
-            order_number: data[i]["order_number"],
-          })
-        }
-      }
-
-      setMasterData(formatedArray)
-    }
-  }, [])
-
+  let temp = []
   const getOrder = () => {
     GET('/orders')
       .then(response => {
-        
-       
-          // console.log("ioioio", data)
-          setMasterData(response.data.response.orders)
-          // setGetOrdersData(data)
-        
+        temp = response.data.response.orders
+        setOrderData(temp)
       })
   }
-   
-  useEffect(()=>{
+
+
+  useEffect(() => {
     getOrder()
-  },[])
-  //  const handlingModal = ()=>{
-  //     setOpeningModal(true)
-  //  }
+    
+  }, [])
 
   return (
     <TableContainer component={Paper}>
@@ -275,54 +155,30 @@ export default function CollapsibleTable() {
         <TableHead>
           <TableRow>
             <TableCell />
-            <TableCell>
-              <b> Contract</b>
-            </TableCell>
-            <TableCell>
-              <b> Customer Number</b>
-            </TableCell>
-            <TableCell>
-              <b>Order Number</b>
-            </TableCell>
-            <TableCell>
-              <b>Hauler Number</b>
-            </TableCell>
-            <TableCell>
-              <b>Driver Number</b>
-            </TableCell>
-            <TableCell>
-              <b>PO Number</b>
-            </TableCell>
-            <TableCell>
-              <b>Supplier Number</b>
-            </TableCell>
-            <TableCell>
-              <b>Terminal Number</b>
-            </TableCell>
-            <TableCell>
-              <b>Order Date</b>
-            </TableCell>
-            <TableCell>
-              <b>Expiration Date</b>
-            </TableCell>
-            {/* <TableCell>
-              <b>Actions</b>
-            </TableCell> */}
+            <TableCell>Order Number</TableCell>
+            <TableCell>Customer Number</TableCell>
+            <TableCell>Customer Name</TableCell>
+            <TableCell>Customer Location</TableCell>
+            <TableCell>PO Number</TableCell>
+            <TableCell>Rack Location</TableCell>
+            <TableCell>Order Date</TableCell>
+            <TableCell>Delivery Date</TableCell>
+            <TableCell>Delivery Window</TableCell>
+            <TableCell>Status</TableCell>
+            <TableCell>Alerts</TableCell>
+            {/* <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
           </TableRow>
         </TableHead>
         <TableBody>
-          {MasterData
-            ? MasterData.map((row, i) =>
-              <TableRow>
-                <TableCell></TableCell>
-              </TableRow>
-              
-            )
-            : null}
-
-
+          {!!orderData && orderData.map((row, i) => (
+            <Row key={i} row={row} />
+            // <h1>{console.log("row", row)}</h1>
+          ))}
         </TableBody>
       </Table>
     </TableContainer>
-  )
+  );
 }
